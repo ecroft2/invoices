db08d6b8e921d91110d8b759e09a93175c60b225
ecroft2 <ecroft2@hotmail.com>
2022-01-17 22:06:03 +0000

    Creates basic invoice data manipulation

---
+++ invoice.js
@@ -0,0 +1,38 @@
+const invoice = (function () {
+    let invoices = [];
+    let counter = 0;
+
+    function generateUniqueId() {
+        counter++;
+        return counter;
+    }
+
+    function addInvoice(data) {
+        invoices.push({
+            id: generateUniqueId(),
+            name: data.name,
+        });
+    }
+
+    function deleteInvoice(id) {
+        invoices = invoices.filter((invoice) => invoice.id !== id);
+    }
+
+    function editInvoice(id, data) {
+        invoices = invoices.map((invoice) =>
+            invoice.id === id
+                ? {
+                      id: invoice.id,
+                      name: data.name,
+                  }
+                : invoice
+        );
+    }
+
+    return {
+        add: addInvoice,
+        delete: deleteInvoice,
+        edit: editInvoice,
+        list: invoices,
+    };
+})();
