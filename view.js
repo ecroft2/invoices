e9231df54f69c704e99bec8e188a30368b786cb1
ecroft2 <ecroft2@hotmail.com>
2022-01-18 21:36:58 +0000

    Creates function to display invoices

--- view.js
+++ view.js
@@ -1,5 +1,29 @@
 const view = (function () {
-    const ul = document.querySelector("invoice-list");
+    const invoiceArea = document.querySelector(".invoices");
 
-    function updateList() {}
+    function displayInvoices(invoices) {
+        invoiceArea.innerHTML = "";
+
+        if (invoices.length === 0) {
+            let p = document.createElement("p");
+            p.textContent = "No invoices!";
+            invoiceArea.append(p);
+        } else {
+            const invoiceList = document.createElement("ul", "invoice-list");
+            invoiceArea.append(invoiceList);
+
+            invoices.forEach((invoice) => {
+                let invoiceListItem = document.createElement(
+                    "li",
+                    "invoice-list-item"
+                );
+                invoiceListItem.textContent = `${invoice.name}`;
+                invoiceList.append(invoiceListItem);
+            });
+        }
+    }
+
+    return {
+        displayInvoices,
+    };
 })();
