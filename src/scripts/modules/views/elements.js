0d4339db141c97d633852e89756c4fb1a39e0612
ecroft2 <ecroft2@hotmail.com>
2022-02-02 23:21:49 +0000

    Temporarily comments out elem builders

--- src/scripts/modules/views/elements.js
+++ src/scripts/modules/views/elements.js
@@ -22,27 +22,48 @@ const createElement = (props) => {
     return fragment.appendChild(element);
 };
 
-const createInputElement = (props) => {
-    let additionalClasses = "input";
-    props.tag = "input";
+// Additional classes on top of additional classes?
+// const createInputElement = (props) => {
+//     let additionalClasses = "input";
+//     props.tag = "input";
 
-    props.className
-        ? (props.className += " " + additionalClasses)
-        : (props.className = additionalClasses);
+//     props.className
+//         ? (props.className += " " + additionalClasses)
+//         : (props.className = additionalClasses);
 
-    return createElement({ ...props });
+//     return createElement({ ...props });
+// };
+
+// const createButtonElement = (props) => {
+//     let additionalClasses =
+//         "group text-xs h-12 p-4 rounded-3xl bg-purple-600 hover:bg-purple-500 font-bold text-white items-center flex";
+//     props.tag = "button";
+
+//     props.className
+//         ? (props.className += " " + additionalClasses)
+//         : (props.className = additionalClasses);
+
+//     return createElement({ ...props });
+// };
+
+// const createInvoiceListElement = (props) => {
+//     let additionalClasses = "mb-16 bg-white p-16 w-full";
+
+//     props.tag = "li";
+//     props.attrs = {
+//         invoiceRole: "list-item",
+//     };
+
+//     props.className
+//         ? (props.className += " " + additionalClasses)
+//         : (props.className = additionalClasses);
+
+//     return createElement({ ...props });
+// };
+
+export {
+    createElement,
+    // createInputElement,
+    // createButtonElement,
+    // createInvoiceListElement,
 };
-
-const createButtonElement = (props) => {
-    let additionalClasses =
-        "group text-xs h-12 p-4 rounded-3xl bg-purple-600 hover:bg-purple-500 font-bold text-white items-center flex";
-    props.tag = "button";
-
-    props.className
-        ? (props.className += " " + additionalClasses)
-        : (props.className = additionalClasses);
-
-    return createElement({ ...props });
-};
-
-export { createElement, createInputElement, createButtonElement };
